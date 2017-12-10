3ece2b073aa8a7264bde353c8409d44b95784abf
Florke07 <maciek.socha1@gmail.com>
2017-12-10 03:14:16 +0100

    coś?

---
+++ core/src/com/redartedgames/libgdxengine2d/utilities/RandomizeRandomTextToAutomat.java
@@ -0,0 +1,18 @@
+package com.redartedgames.libgdxengine2d.utilities;
+
+import java.util.ArrayList;
+
+public class RandomizeRandomTextToAutomat {
+
+    ArrayList<String> teksty;
+
+    RandomizeRandomTextToAutomat(){
+
+    }
+
+    String getRandomTekst (){
+
+
+        return "penis";
+    }
+}
