9afd702d61c99b9c0e62e57236fa7f3bbd67de42
Wredter <wiktor.muraszko@wp.pl>
2017-12-10 02:49:38 +0100

    Auto stash before merge of "master" and "origin/master"

--- core/src/com/redartedgames/libgdxengine2d/assets/GuardAnimation.java
+++ core/src/com/redartedgames/libgdxengine2d/assets/GuardAnimation.java
@@ -1,5 +1,6 @@
 package com.redartedgames.libgdxengine2d.assets;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.redartedgames.libgdxengine2d.main.GameObject;
 import com.redartedgames.libgdxengine2d.main.Hitbox;
@@ -10,10 +11,9 @@ public class GuardAnimation extends GameObject{
 	public SpriteObject guardspritenotWalking;
 	public SpriteObject guardspriteisWalking;
 	public Vector2 mov;
-	public int alfa;
-	public SpriteObject guardhead;
+	public float alfa;
+	public SpriteObject guardhead, guardhead360;
 	public GameObject guadHitbox;
-	public boolean isWalking = false;
 	public GuardAnimation(float x, float y, GameObject parent, boolean isAttached) {
 		super(x, y, parent, isAttached);
 		// TODO Auto-generated constructor stub
@@ -45,30 +45,68 @@ public class GuardAnimation extends GameObject{
 		guardhead.addTexture("graphic/plajer/glowa_ob2.png");
 		guardhead.addTexture("graphic/plajer/glowa_ob2_5.png");
 		
+		guardhead360 = new SpriteObject(0,0,this,true);
+		addSprite(guardhead360);
+		guardhead360.visibility=0;
+		guardhead360.addTexture("graphic/plajer/glowa360/1.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/2.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/3.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/4.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/5.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/6.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/7.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/8.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/9.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/10.png");
+		guardhead360.addTexture("graphic/plajer/glowa360/11.png");
+	
 	}
-	public void beginWalking(Vector2 mov){
+	public void beginWalking(){
+		
 		guardspritenotWalking.visibility=0;
-		this.movement.setVelocity(mov);
+		guardspriteisWalking.alfa=this.movement.getVelocity().angle();
 		guardspriteisWalking.visibility= 1;
 	}
 	public void endWalking(){
 		guardspriteisWalking.visibility= 0;
-		this.movement.setVelocity(new Vector2(0,0));
+		movement.setVelocity(new Vector2(0,0));
 		guardspritenotWalking.visibility=1;
 	}
-	public void setalfa(int alfa){
+	public void setalfa(float alfa){
 		guardspriteisWalking.alfa = alfa;
 		guardspritenotWalking.alfa = alfa;
 		guardhead.alfa =alfa;
+		guardhead360.alfa = alfa;
 	}
-	int dupa=0;
-	 public void update (float delta){
+	float wdupe;
+	
+	public void update (float delta){
 		super.update(delta);
+		wdupe += Gdx.graphics.getDeltaTime();
 		delta *= 50;
 		movement.updateAfter(delta/40);
-		guardspriteisWalking.updateFrames(delta/50);
-		guardspritenotWalking.updateFrames(delta/40);
-		guardhead.updateFrames(delta/20);
+		beginWalking();
+		guardspriteisWalking.updateFrames(delta/20);
+		guardspriteisWalking.alfa=(parent.movement.getVelocity().angle()+180);
+		guardhead.alfa=(parent.movement.getVelocity().angle()+180);
+		guardhead360.alfa=(parent.movement.getVelocity().angle()+180);
+		//guardspritenotWalking.updateFrames(delta);
+		guardhead360.updateFrames(delta/2);
+		
+		if(wdupe >= 5){
+			guardhead.isVisible = false;
+			guardhead360.visibility = 1;
+			
+			System.out.println("dupaadupa" + wdupe);
+			if(wdupe>=5.25){
+				guardhead.isVisible = true;
+				guardhead360.visibility = 0;
+				wdupe = 0;
+			}
+		}
+		else
+			guardhead.updateFrames(delta/20);
+		
 		System.out.println("pos x= " + guadHitbox.getRealpositionX(0) + " pos y= "+ guadHitbox.getRealpositionY(0));
 	}
 	//TODO Dopisaæ get hitbox 
