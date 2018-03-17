33311768aa24b60198baa9401b197a40b1d7ada5
Florke07 <maciek.socha1@gmail.com>
2018-03-17 19:13:33 +0100

    htiboxxxy

--- core/src/com/redartedgames/libgdxengine2d/assets/ElecrticalElementSprite.java
+++ core/src/com/redartedgames/libgdxengine2d/assets/ElecrticalElementSprite.java
@@ -3,14 +3,18 @@ package com.redartedgames.libgdxengine2d.assets;
 import com.badlogic.gdx.graphics.Texture;
 import com.redartedgames.libgdxengine2d.gameobject.GameObject;
 import com.redartedgames.libgdxengine2d.gameobject.SpriteObject;
+import com.redartedgames.libgdxengine2d.physics.Hitbox;
 
 public class ElecrticalElementSprite extends SpriteObject{
 
     private Texture txt;
+    private Hitbox hit;
+
     public ElecrticalElementSprite(float x, float y, GameObject parent, boolean isAttached, int type) {
         super(x, y, parent, isAttached);
         //addTexture("graphic/ukladyElektryczne/u" + type +".png");
         txt = new Texture("graphic/ukladyElektryczne/u" + type +".png");
+        hit = new Hitbox(x,y,txt.getWidth(),txt.getHeight(), Hitbox.BehaviorMode.kinematic);
 
         switch (type) {
             case 0: //dioda LED
