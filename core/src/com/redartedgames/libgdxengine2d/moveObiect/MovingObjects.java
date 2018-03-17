bbb439efc469158257f6f367c8ceffa4c00cabdd
komp\Wojtek <w.mosienski@gmail.com>
2018-03-17 09:24:31 +0100

    comit abc

--- core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
+++ core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
@@ -4,13 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.redartedgames.libgdxengine2d.physics.CollisionHandle;
 
 public class MovingObjects {
-
+    private static float timeOfFullAnimacion;
+    private static float movingMulitilplier;
 
     public static CollisionHandle animationUpAndDown(float timeOfMoving) {
         //-t(t-d)*k
         CollisionHandle finalPositionOfObject = new CollisionHandle();
-        float timeOfFullAnimacion=50.0f;
-        float movingMulitilplier=1.0f;
+        timeOfFullAnimacion=5.0f;
+        movingMulitilplier=1.0f;
 
         finalPositionOfObject.disX=0;
         if (timeOfMoving>=timeOfFullAnimacion) {
@@ -26,8 +27,8 @@ public class MovingObjects {
     public static CollisionHandle animationCurve(float timeOfMoving) {
         CollisionHandle finalPositionOfObject = new CollisionHandle();
         //t(t-d)*k
-        float timeOfFullAnimacion=5.0f; //d
-        float movingMulitilplier=1.0f; //k
+        timeOfFullAnimacion=5.0f; //d
+        movingMulitilplier=1.0f; //k
 
         if (timeOfMoving>=timeOfFullAnimacion) {
             finalPositionOfObject.disY=0;
