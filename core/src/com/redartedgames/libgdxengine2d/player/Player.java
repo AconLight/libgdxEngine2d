a40e7ecaf777da775a5d0fc18a4e36f47b5782da
komp\Wojtek <w.mosienski@gmail.com>
2018-03-17 12:29:50 +0100

    player

---
+++ core/src/com/redartedgames/libgdxengine2d/player/Player.java
@@ -0,0 +1,51 @@
+package com.redartedgames.libgdxengine2d.player;
+
+import java.util.ArrayList;
+
+import com.redartedgames.libgdxengine2d.assets.PlayerSprite;
+import com.redartedgames.libgdxengine2d.formation.Formation;
+import com.redartedgames.libgdxengine2d.formation.MyFormationGenerator;
+import com.redartedgames.libgdxengine2d.gameobject.GameObject;
+import com.redartedgames.libgdxengine2d.smartphones.SmartphoneBlue;
+import com.redartedgames.libgdxengine2d.smartphones.SmartphoneRed;
+import com.redartedgames.libgdxengine2d.smartphones.SmartphoneYellow;
+
+public class Player extends GameObject{
+
+	Formation formation;
+	PlayerSprite sprite;
+	
+	public Player(float x, float y, GameObject parent, boolean isAttached) {
+		super(x, y, parent, isAttached);
+		ArrayList<GameObject> objects = new ArrayList<>();
+		
+		objects.add(new SmartphoneBlue(0, 0, this));
+		objects.add(new SmartphoneRed(0, 0, this));
+		objects.add(new SmartphoneRed(0, 0, this));
+		objects.add(new SmartphoneBlue(0, 0, this));
+		objects.add(new SmartphoneYellow(0, 0, this));
+		
+		((SmartphoneYellow)objects.get(4)).addRed((SmartphoneRed) objects.get(1));
+		((SmartphoneYellow)objects.get(4)).addRed((SmartphoneRed) objects.get(2));
+		
+		collidableObjects.add(this); // just to asure that collide is performed
+		
+
+		formation = new Formation(0, 0, objects, this, true, new MyFormationGenerator());
+		
+		gameObjects.addAll(objects);
+		gameObjects.add(formation);
+		
+		sprite = new PlayerSprite(0, 0, this, true);
+		gameObjects.add(sprite);
+	}
+	public void collide(GameObject obj) {
+		if (obj == this) {
+			formation.collide(obj);
+		}
+		else {
+			super.collide(obj);
+		}
+	}
+
+}
