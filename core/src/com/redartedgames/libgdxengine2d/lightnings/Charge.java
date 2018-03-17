53b33e475f2ac50db3516e8e69c7ad81a02da213
Stachu <s.robot@wp.pl>
2018-03-17 01:13:24 +0100

    chaRGE

--- core/src/com/redartedgames/libgdxengine2d/lightnings/Charge.java
+++ core/src/com/redartedgames/libgdxengine2d/lightnings/Charge.java
@@ -1,13 +1,33 @@
 package com.redartedgames.libgdxengine2d.lightnings;
 
-public class Charge {
+import com.badlogic.gdx.graphics.Texture;
+import com.redartedgames.libgdxengine2d.gameobject.GameObject;
+import com.redartedgames.libgdxengine2d.gameobject.SpriteObject;
+
+public class Charge extends GameObject {
+
     private float x;
     private float y;
     private float size;
-    public Charge(float x, float y, float size) {
+    private SpriteObject chargeSprite;
+
+    public Charge(float x, float y, float size, GameObject parent, boolean isAttached) {
+        super(x,y,parent,isAttached);
         this.x = x;
         this.y = y;
         this.size = size;
+        chargeSprite = new SpriteObject(0,0,this,true);
+        addSprite(chargeSprite);
+        Texture texture = new Texture("graphic/charge/charge.png");
+        scale(chargeSprite,texture);
+        chargeSprite.addTexture(texture);
+        chargeSprite.visibility = 1;
+    }
+
+    private void scale(SpriteObject s, Texture t) {
+        float scale = t.getHeight()/size;
+        s.sclX = scale;
+        s.sclY = scale;
     }
 
     public float getX() {
@@ -20,5 +40,10 @@ public class Charge {
 
     public float getSize() {
         return size;
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
     }
 }
--- core/src/com/redartedgames/libgdxengine2d/lightnings/Lightning.java
+++ core/src/com/redartedgames/libgdxengine2d/lightnings/Lightning.java
@@ -1,5 +1,82 @@
 package com.redartedgames.libgdxengine2d.lightnings;
 
-public class Lightning {
+import com.badlogic.gdx.graphics.Texture;
+import com.redartedgames.libgdxengine2d.gameobject.GameObject;
 
+import java.util.ArrayList;
+
+public class Lightning extends GameObject{
+
+    private float startX;
+    private float startY;
+    private float endX;
+    private float endY;
+    private float chargeSize;
+    private boolean wasStarted;
+    private boolean wasStopped;
+    private int animationCounter;
+    private int amount;
+    private float distance;
+    public ArrayList<Charge> charges;
+
+    public Lightning(float startX, float startY, float endX, float endY, GameObject parent, boolean isAttached) {
+        super(startX,startY,parent, isAttached);
+
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+        charges = new ArrayList<>();
+        chargeSize = calculateSize();
+        animationCounter = 0;
+        amount = calculateAmount();
+        distance = calculateDistance();
+        addCharges();
+        //Gdx.graphics.getDeltaTime();
+    }
+
+    private float calculateDistance() {
+        return (float)(Math.sqrt(Math.pow((startX-endX),2)+Math.pow((startY-endY),2)));
+    }
+
+    private int calculateAmount() {
+        Texture texture = new Texture("graphic/charge/charge.png");
+        float f = calculateDistance()/texture.getHeight();
+        if(f != (int)f) f+=1;
+        return (int)f;
+    }
+
+    private float calculateSize() {
+        return (int)calculateDistance()/calculateAmount();
+    }
+
+    private void addCharges() {
+        for(int i = 0; i < amount; i++) {
+            float x = startX + (endX-startX)*(amount*2-(i*2+1))/(amount*2);//((((startX-endX)/(float)(2.0*amount))*i*2+1));
+            float y = startY + (startY-endY)*(amount*2-(i*2+1))/(amount*2);//((((startY-endY)/(float)(2.0*amount))*i*2+1));
+            charges.add(new Charge(x,y,chargeSize,this,true));
+        }
+    }
+
+    public void start() {
+        if(!wasStarted && !wasStopped) {
+            wasStarted = true;
+            animationCounter = 0;
+        }
+    }
+
+    public void stop() {
+        if(wasStarted && !wasStopped) wasStopped = true;
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        if(wasStarted && !wasStopped) {
+
+        }
+        else if(wasStopped) {
+
+        }
+    }
 }
