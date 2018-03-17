ef9325c0753f6013cb55a56ed66fa0a9460d64c7
Florke07 <maciek.socha1@gmail.com>
2018-03-17 03:06:22 +0100

    animacja po curvie

--- core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
+++ core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
@@ -1,18 +1,22 @@
 package com.redartedgames.libgdxengine2d.moveObiect;
 
+import com.badlogic.gdx.Gdx;
 import com.redartedgames.libgdxengine2d.physics.CollisionHandle;
+
+import java.awt.event.FocusAdapter;
 
 public class MovingObjects {
     private static float timeOfFullAnimacion;
     private static float movingMulitilplier;
 
     public static CollisionHandle animationUpAndDown(float timeOfMoving) {
+        //-t(t-d)*k
         CollisionHandle finalPositionOfObject = new CollisionHandle();
         timeOfFullAnimacion=5.0f;
         movingMulitilplier=1.0f;
 
         finalPositionOfObject.disX=0;
-        if (timeOfMoving>timeOfFullAnimacion) {
+        if (timeOfMoving>=timeOfFullAnimacion) {
             finalPositionOfObject.disY=0;
         } else {
             finalPositionOfObject.disY = Math.abs(-timeOfMoving * (timeOfMoving - timeOfFullAnimacion) * movingMulitilplier);
@@ -24,6 +28,21 @@ public class MovingObjects {
 
     public static CollisionHandle animationCurve(float timeOfMoving) {
         CollisionHandle finalPositionOfObject = new CollisionHandle();
+        //t(t-d)*k
+        timeOfFullAnimacion=5.0f; //d
+        movingMulitilplier=1.0f; //k
+
+        if (timeOfMoving>=timeOfFullAnimacion) {
+            finalPositionOfObject.disY=0;
+            finalPositionOfObject.disX=0;
+        } else {
+            finalPositionOfObject.disX = timeOfMoving;
+            finalPositionOfObject.disY = Math.abs(-timeOfMoving * (timeOfMoving - timeOfFullAnimacion) * movingMulitilplier);
+        }
+
+        Gdx.app.log("czas:",Float.toString(timeOfMoving));
+        Gdx.app.log("X:", Float.toString(finalPositionOfObject.disX));
+        Gdx.app.log("Y:",Float.toString(finalPositionOfObject.disY));
 
         return finalPositionOfObject;
     }
