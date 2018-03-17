61cfde44b835a285d2fee2af2c12bc136a069801
Florke07 <maciek.socha1@gmail.com>
2018-03-17 16:39:06 +0100

    jest pasek życia

--- core/src/com/redartedgames/libgdxengine2d/player/LifeBelt.java
+++ core/src/com/redartedgames/libgdxengine2d/player/LifeBelt.java
@@ -1,24 +1,19 @@
 package com.redartedgames.libgdxengine2d.player;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.redartedgames.libgdxengine2d.gameobject.GameObject;
 import com.redartedgames.libgdxengine2d.gameobject.SpriteObject;
 
-import java.lang.reflect.GenericDeclaration;
-
 public class LifeBelt extends SpriteObject{
 
     private float x;
     private float y;
-    //private GameObject parent;
     private float pointsOfLife=10;
     public LifeBelt(float x, float y, GameObject parent, boolean isAttached) {
         super(x, y, parent, isAttached);
         this.x = x;
         this.y = y;
-        //this.parent=parent;
     }
 
     public void render(ShapeRenderer batch, int priority, float dx, float dy,float visibility) {
@@ -27,10 +22,7 @@ public class LifeBelt extends SpriteObject{
     }
 
     public void updateLast(float delta, float vx, float vy) {
-        //pointsOfLife=Player.urLife;
-        pointsOfLife=((Player)parent).urLife;
-        Gdx.app.log("KURWAAAAAAAAAAAAAA",Float.toString(pointsOfLife));
-        //super.updateLast(delta,vx,vy);
+        //pointsOfLife=((Player)parent).urLife;
     }
 
 
