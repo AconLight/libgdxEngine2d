18732d374473a994e20c2800f745a95978f037a6
Florke07 <maciek.socha1@gmail.com>
2018-03-17 02:33:53 +0100

    MovingObj ma funkcje kwadratowom

--- core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
+++ core/src/com/redartedgames/libgdxengine2d/moveObiect/MovingObjects.java
@@ -1,4 +1,27 @@
 package com.redartedgames.libgdxengine2d.moveObiect;
 
+import com.redartedgames.libgdxengine2d.physics.CollisionHandle;
+
 public class MovingObjects {
+    private static float timeOfFullAnimacion;
+    private static float movingMulitilplier;
+
+    public static CollisionHandle animationPosition(float timeOfMoving) {
+        CollisionHandle finalPositionOfObject = new CollisionHandle();
+        timeOfFullAnimacion=5.0f;
+        movingMulitilplier=1.0f;
+
+        finalPositionOfObject.disX=0;
+        if (timeOfMoving>timeOfFullAnimacion) {
+            finalPositionOfObject.disY=0;
+        } else {
+            finalPositionOfObject.disY = Math.abs(-timeOfMoving * (timeOfMoving - timeOfFullAnimacion) * movingMulitilplier);
+        }
+
+
+        return finalPositionOfObject;
+
+
+
+    }
 }
